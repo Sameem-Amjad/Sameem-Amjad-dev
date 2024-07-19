e74af7222a92c1c1698ae714fdc58ca2f1a99d29
Sameem_Amjad <92137575+Sameem-Amjad@users.noreply.github.com>
2024-07-20 02:18:18 +0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,7 +31,7 @@ const About = () =>
             {/* about me */ }
             <div>
             <h2 className='h2 text-accent'>About me.</h2>
-            <h3 className='h3 mb-4 lg:mx-0'>I'm a Freelancer Front-end Developer with over 2 years experience.</h3>
+            <h3 className='h3 mb-4 lg:mx-0'>I'm a Freelancer Front-end Developer with over 4 years experience.</h3>
             <p className='mb-8 lg:mx-0'>
              I'm known for building stunning and captivating Websites & Mobile applications for both businesses and individuals and also I have a team of the best & experienced developers and designers I would love to help you with your desire Website & Mobile applications project.
             </p>
@@ -41,7 +41,7 @@ const About = () =>
                 <div className='text-[40px] text-tertiary animate-pulse text-gradient p-2 
                 border-purple-400 focus:ring-2 focus:ring-purple-400' >
                   {/* <h1>{useCountUp({ end: 13, duration: 3 }) }</h1> */ }
-                  { inView ? <CountUp end={ 2 } duration={ 5 } /> : null }
+                  { inView ? <CountUp end={ 4 } duration={ 5 } /> : null }
                 </div>
                 <div className='font-secondary text-sm tracking-[2px]'>Years of <br />Experience</div>
               </div>
@@ -58,7 +58,7 @@ const About = () =>
                 <div className='text-[40px] text-tertiary animate-pulse text-gradient p-2
                 border-purple-400 focus:ring-2 focus:ring-purple-400' >
                   {/* <h1>{useCountUp({ end: 13, duration: 3 }) }</h1> */ }
-                  { inView ? <CountUp end={ 46 } duration={ 5 } /> : null }
+                  { inView ? <CountUp end={ 50 } duration={ 5 } /> : null }
                   +
                 </div>
                 <div className='font-secondary text-sm tracking-[2px]'>Client <br />Satisfied</div>
